3f00d6338d625671e8dffdb87e293b33dd6f504b
Alisdair Rankine <info@alisdairrankine.com>
2015-11-24 19:06:32 +0000

    Initial Commit

---
+++ Space/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Space
+//
+//  Created by Ali on 24/11/2015.
+//  Copyright © 2015 Siege Perilous Studios. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
